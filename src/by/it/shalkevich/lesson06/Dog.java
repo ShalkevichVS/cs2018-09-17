0cc4d847b7fa9512f575cba04cf69fb82c689b97
mastervlad <mastervladsh@gmail.com>
2018-09-28 12:16:16 +0300

    lesson3

---
+++ src/by/it/shalkevich/lesson06/Dog.java
@@ -0,0 +1,33 @@
+package by.it.shalkevich.lesson06;
+
+public class Dog {
+    private String name;
+    private int age;
+
+    public Dog() {
+    }
+
+    public Dog(String барбос, int i) {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Кличка: " + name + ". Возраст: " + age;
+    }
+}
